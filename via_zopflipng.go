b463f5d18983d4cf503f470183876319f304eda7
metaleap <metaleap@users.noreply.github.com>
2021-07-17 11:43:37 +0200

    [autopush] «FUN is never having to say you're SUSHI!!»

--- via_zopflipng.go
+++ via_zopflipng.go
@@ -16,15 +16,18 @@ func init() {
 func viaZopfli(srcFilePath string, srcFileLen int, printMsg func(...interface{})) []byte {
 	iter := "444"
 	if srcFileLen > (32 * 1024) {
-		iter = "222"
+		iter = "333"
 	}
 	if srcFileLen > (128 * 1024) {
-		iter = "88"
+		iter = "222"
 	}
 	if srcFileLen > (640 * 1024) {
-		iter = "22"
+		iter = "111"
 	}
-	if srcFileLen > (5 * 1024 * 1024) {
+	if srcFileLen > (8 * 1024 * 1024) {
+		iter = "11"
+	}
+	if srcFileLen > (32 * 1024 * 1024) {
 		iter = "4"
 	}
 	return viaCmd(printMsg, nil, "zopflipng", "-m", "--lossy_transparent", "--lossy_8bit", "--filters=01234mepb", "--iterations="+iter,
