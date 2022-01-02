fabfc6ddb7a5eaf917b7175a4c5aff8c3a798b4c
metaleap <metaleap@users.noreply.github.com>
2022-01-02 08:43:19 +0100

    [autopush] «We just joined the civil hair patrol!»

--- via_zopflipng.go
+++ via_zopflipng.go
@@ -21,14 +21,14 @@ func viaZopfli(srcFilePath string, srcFileLen int, printMsg func(...interface{})
 	if srcFileLen > (128 * 1024) {
 		iter = "77"
 	}
-	if srcFileLen > (256 * 1024) {
+	if srcFileLen > (216 * 1024) {
 		iter = "55"
 	}
-	if srcFileLen > (320 * 1024) {
+	if srcFileLen > (304 * 1024) {
 		iter = "44"
 	}
-	if srcFileLen > (384 * 1024) {
-		iter = "33"
+	if srcFileLen > (376 * 1024) {
+		iter = "22"
 	}
 	if srcFileLen > (528 * 1024) {
 		iter = "11"
@@ -42,7 +42,7 @@ func viaZopfli(srcFilePath string, srcFileLen int, printMsg func(...interface{})
 	if srcFileLen > (896 * 1024) {
 		iter = "2"
 	}
-	if srcFileLen > (1024 * 1024) {
+	if srcFileLen > (1016 * 1024) {
 		iter = "1"
 	}
 	return viaCmd(printMsg, nil, "zopflipng", "-m", "--lossy_transparent", "--lossy_8bit", "--filters=01234mepb", "--iterations="+iter,
